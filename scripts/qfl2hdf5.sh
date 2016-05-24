c708e75c334d1e072fb028b774b5f1f4ba39d507
Johannes Markert <johannes.markert@gmail.com>
2016-05-24 21:43:22 +0200

    added PARALLEL cmd

--- scripts/qfl2hdf5.sh
+++ scripts/qfl2hdf5.sh
@@ -3,6 +3,8 @@
 SPATH="${BASH_SOURCE[0]}"
 PPATH="$(dirname "${BASH_SOURCE[0]}")"
 
+PARALLEL="~/frameworks/parallel/bin/parallel"
+
 DIR="$1"
 
 SPT="$DIR/out/snapshot_hdf5_plt_cnt_*"
@@ -10,14 +12,14 @@ OUT="$DIR/hdf5"
 LOG="$DIR/log/parallel-hdf5-$(date +%Y-%m-%d)"
 
 # bouchut
-DBS="dens temp pres velx vely velz accx accy accz magx magy magz magp"
+# DBS="dens temp pres velx vely velz accx accy accz magx magy magz magp"
 
 # 8wave
-# DBS="dens velx vely velz accx accy accz magx magy magz"
+DBS="dens velx vely velz accx accy accz magx magy magz"
 
 rm -f "$LOG"
 mkdir -p "$OUT"
 
-find $SPT | parallel \
+find $SPT | $PARALLEL \
     --joblog $LOG \
     "$PPATH/../bin/qfl2hdf5 {} $OUT/{/} $DBS && echo '{/} finnished!'"
