9a48c5b0e5e0a4b3aca317adc534736c5b335652
Johannes Markert <johannes.markert@gmail.com>
2016-05-28 16:52:21 +0200

    first production ready version

---
+++ src/spectrum.cpp
@@ -0,0 +1,164 @@
+#include <string>
+#include <vector>
+#include <iostream>
+#include <stdlib.h>
+#include <cmath>
+
+# include "H5File.hpp"
+# include "H5DataSet.hpp"
+# include "H5DataSpace.hpp"
+# include "H5DataType.hpp"
+
+# include "Array.h"
+# include "fftw++.h"
+
+using std::cout;
+using std::cerr;
+using std::endl;
+
+using namespace std;
+using namespace utils;
+using namespace Array;
+using namespace fftwpp;
+
+typedef unsigned int uint;
+typedef std::vector<double> dvec;
+
+double norm3d (const double x, const double y, const double z)
+{
+    return std::sqrt( x*x + y*y + z*z );
+}
+
+double norm3d (const dvec & v)
+{
+    return std::sqrt( v[0]*v[0] + v[1]*v[1] + v[2]*v[2] );
+}
+
+template <typename T>
+void read_ds
+(
+    const HighFive::File &file,
+    const std::string &dname,
+    std::vector<T> &vec
+)
+{
+    const auto &dataset = file.getDataSet(dname);
+    dataset.read(vec);
+}
+
+template <typename T>
+void read_ds (
+    const HighFive::File &file,
+    const std::string &dname,
+    Array::array3<T> &arr
+)
+{
+    const auto &dataset = file.getDataSet(dname);
+
+    std::vector<T> tmp;
+    dataset.read(tmp);
+
+    arr = tmp.data();
+}
+
+template <typename T>
+void write_ds (
+    HighFive::File &file,
+    const std::string &dname,
+    const Array::array3<T> &arr
+) {
+    const HighFive::DataSpace Nxyz (arr.Nx()*arr.Ny()*arr.Nz());
+    T *data = &*arr;
+    file.createDataSet<T>(dname, Nxyz).write( data );
+}
+
+template <typename T>
+void write_ds (
+    HighFive::File &file,
+    const std::string &dname,
+    const std::vector<T> &vec
+) {
+    file.createDataSet<T>(dname, HighFive::DataSpace::From(vec))
+        .write(const_cast<std::vector<T>&>(vec));
+}
+
+int main(int argc, char * argv[])
+{
+    // ---------------------------------------------------------------------- //
+    // Read in Commandline args
+
+    if (argc < 4)
+    {
+        cerr    << "Usage: " << argv[0] 
+                << " <input file>" 
+                << " <output file>" 
+                << " <dbname> <dbname> ..." 
+                << endl;
+        return 1;
+    }
+
+    int argPtr = 1;
+    const std::string input_file_name = argv[argPtr++];
+    const std::string output_file_name = argv[argPtr++];
+
+    std::vector<std::string> dbnames;
+    while (argPtr < argc)
+        dbnames.push_back(argv[argPtr++]);
+
+    // ---------------------------------------------------------------------- //
+    // Initialize arrays for FFTW++
+
+    // Open input file
+    const HighFive::File infile(input_file_name, HighFive::File::ReadOnly);
+    
+    // Open output file
+    HighFive::File outfile(output_file_name, 
+          HighFive::File::ReadWrite 
+        | HighFive::File::Create 
+        | HighFive::File::Truncate );
+
+    // META DATA
+    std::vector<uint> dims(3);
+    read_ds(infile,"DIMS",dims);
+
+    std::vector<uint> fdims(3);
+    fdims[0] = dims[0];
+    fdims[1] = dims[1];
+    fdims[2] = dims[2]/2+1;
+
+    write_ds(outfile, "DIMS" ,fdims);
+
+    // fftw::maxthreads = get_max_threads();
+    fftw::maxthreads = 1;
+
+    for ( std::string &dbname : dbnames )
+    {
+        array3<double>  real        (dims[0],dims[1],dims[2]);
+        array3<Complex> fourier     (fdims[0],fdims[1],fdims[2]);
+        array3<double>  fourier_abs (fdims[0],fdims[1],fdims[2]);
+        array3<double>  fourier_arg (fdims[0],fdims[1],fdims[2]);
+
+        read_ds(infile,dbname,real);
+
+        // prepare process
+        rcfft3d fforward(dims[0],dims[1],dims[2],real,fourier);
+
+        // compute Fourier Spectra
+        fforward.fft0Normalized(real,fourier);
+
+        // compute abs and arg of fourier
+        for (uint i = 0 ; i < fdims[0] ; i++)
+        for (uint j = 0 ; j < fdims[1] ; j++)
+        for (uint k = 0 ; k < fdims[2] ; k++)
+        {
+            fourier_abs(i,j,k) = std::abs(fourier(i,j,k));
+            fourier_arg(i,j,k) = std::arg(fourier(i,j,k));
+        }
+
+        // write results to output file
+        write_ds(outfile, dbname + "_abs", fourier_abs);
+        write_ds(outfile, dbname + "_arg", fourier_arg);
+    }
+
+    return 0;
+}
