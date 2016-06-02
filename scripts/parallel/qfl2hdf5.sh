99f5c1a286d3c1b64732789b5620f1fc5f77f576
Johannes Markert <johannes.markert@gmail.com>
2016-06-02 16:49:02 +0200

    waaay better parallel scriptsgit add .

--- scripts/parallel/qfl2hdf5.sh
+++ scripts/parallel/qfl2hdf5.sh
@@ -1,31 +1,35 @@
 #!/usr/bin/env bash
 
-PROJECT_DIR="/srv/projects/astro/toolbox/turbubox"
-
-#PARALLEL_CMD="~/frameworks/parallel/bin/parallel"
-PARALLEL_CMD="parallel"
-
-PWD_DIR="${1:?No working directory given!}"
-SRC_DIR="$PWD_DIR/amr" # adaptive mesh refinement (FLASH file format)
-OUT_DIR="$PWD_DIR/ugm" # uniform grid mesh (my file format)
-LOG_DIR="$PWD_DIR/log" # logging information
-LOG_FILE="${LOG_DIR}/qfl2hdf5_$(date +%Y-%m-%d-%H-%M-%S)"
+export PRJ_DIR="$(dirname "${BASH_SOURCE[0]}")/../.."
+export PWD_DIR="${1:?No working directory given!}"
+export SRC_DIR="$PWD_DIR/amr" # adaptive mesh refinement (FLASH file format)
+export OUT_DIR="$PWD_DIR/ugm" # uniform grid mesh (my file format)
+export LOG_DIR="$PWD_DIR/log" # logging information
+export LOG_FIL="${LOG_DIR}/qfl2hdf5_$(date +%Y-%m-%d-%H-%M-%S)"
 
 if [ -v DRYRUN ]
 then
-    DRYRUN="--dry-run"
+    export DRYRUN="--dry-run"
+else
+    mkdir -v -p "$OUT_DIR"
+    mkdir -v -p "$LOG_DIR"
 fi
 
-DATASETS="dens velx vely velz accx accy accz magx magy magz"
+export DATASETS="dens velx vely velz accx accy accz magx magy magz"
 
-CMD=$(cat <<end
-${PROJECT_DIR}/bin/qfl2hdf5 {} $OUT_DIR/{/} $DATASETS
-&& echo '{/} finnished!'
-end
-)
+process()
+{
+    local SRC_FILE="$1"
+    local OUT_FILE="$OUT_DIR/$(basename $SRC_FILE)"
+    
+    $PRJ_DIR/bin/qfl2hdf5 $SRC_FILE $OUT_FILE $DATASETS \
+    && echo "$SRC_FILE finnished!"
+}
 
-mkdir -p "$OUT_DIR"
-mkdir -p "$LOG_DIR"
+readonly -f process
+export -f process
 
-find $SRC_DIR/flash_hdf5_plt_cnt_* -type f \
-| ${PARALLEL_CMD} --joblog $LOG_FILE $DRYRUN $CMD
+find $SRC_DIR -type f \
+    | grep -P '/\d{4}$' \
+    | sort \
+    | parallel --joblog $LOG_FIL $DRYRUN process
