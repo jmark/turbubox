bccf377c3c0a6569c9b8b887c8aa5a792557471d
Johannes Markert <jmarker2@uni-koeln.de>
2016-08-28 11:40:34 +0200

    tidy up setup/ and added refined batch files

--- setup/setup.sh
+++ setup/setup.sh
@@ -16,6 +16,6 @@ solverUnit['es']='--with-unit=physics/Hydro/HydroMain/split/ES'
 ./setup \
     Girichidis-StirTurb \
     -3d -nxb=$NX -nyb=$NX -nzb=$NX \
-    -maxblocks=200 -auto -portable -opt -site="$site" \
+    -maxblocks=400 -auto -portable -opt -site="$site" \
     ${solverUnit[$solver]} \
     -objdir="$objdir"
