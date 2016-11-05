7a86f36065b4c0341564d558a384f9f4bcdf8280
Johannes Markert <johannes.markert@gmail.com>
2016-11-05 11:03:42 +0100

    removed vec3D module from evolution.py

--- tools/bin/evolution.py
+++ tools/bin/evolution.py
@@ -4,7 +4,7 @@ import numpy as np
 import sys
 
 import flash as fl
-import vectoranalysis3D as va3D
+import ulz
 
 for count,filepath in enumerate(sys.stdin):
     flash = fl.File(filepath.rstrip())
@@ -25,9 +25,9 @@ for count,filepath in enumerate(sys.stdin):
 
     density   = flash.data('dens')
     velocity  = [flash.data('vel'+dim) for dim in 'x y z'.split()]
-    vorticity = va3D.curl(velocity[0],velocity[1],velocity[2],CS[0],CS[1],CS[2])
+    vorticity = ulz.curl(velocity[0],velocity[1],velocity[2],CS[0],CS[1],CS[2])
 
-    ekintotal = Vcell/2.0     * np.sum(density * va3D.norm(velocity[0],velocity[1],velocity[2]))
-    evortotal = CS[0]**5/12.0 * np.sum(density * va3D.norm(vorticity[0],vorticity[1],vorticity[2]))
+    ekintotal = Vcell/2.0     * np.sum(density * ulz.norm(velocity[0],velocity[1],velocity[2]))
+    evortotal = CS[0]**5/12.0 * np.sum(density * ulz.norm(vorticity[0],vorticity[1],vorticity[2]))
 
     print("\t".join(map(str,[count,step,time,ekintotal,evortotal])), flush=True)
