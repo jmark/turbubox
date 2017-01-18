93c5b16a927de35aafbaef3470e80c5e3fea5357
jmark <me@jmark.de>
2017-01-18 13:27:56 +0100

    merged flash2flash script

--- tools/bin/flash_to_flash.py
+++ tools/bin/flash_to_flash.py
@@ -13,19 +13,11 @@ import scipy.ndimage
 # =========================================================================== #
 
 def zoom(src):
-<<<<<<< HEAD
     zoomfactor = 1/2
     return scipy.ndimage.interpolation.zoom(src, zoomfactor, order=1, mode='wrap')
 
 def scale(src):
     scalefactor = 1
-=======
-    zoomfactor = 2
-    return scipy.ndimage.interpolation.zoom(src, zoomfactor, order=1, mode='wrap')
-
-def scale(src):
-    scalefactor = 0.4
->>>>>>> 41bfc6d777e409b93deb5fd8727c227541b17906
     return scalefactor * src
 
 def blur(src):
@@ -43,7 +35,6 @@ print("  ------|----------------------------|----------------------------")
 
 with flash.File(srcfp,mode='r') as srcfls:
     with flash.File(snkfp,mode='r+') as snkfls:
-<<<<<<< HEAD
         srcdens, srcvelx, srcvely, srcvelz, srcpres = srcfls.get_prims()
         srcener = srcfls.get_data('ener')
 
@@ -58,19 +49,6 @@ with flash.File(srcfp,mode='r') as srcfls:
 
         srcs = [srcdens, srcvelx, srcvely, srcvelz, srcpres, srcener]
         snks = [snkdens, snkvelx, snkvely, snkvelz, snkpres, snkener]
-=======
-        for dbname in 'dens velx vely velz pres'.split():
-            src = srcfls.get_data(dbname)
-            tmp = src
-
-            tmp = zoom(tmp)
-
-            # if dbname in "dens pres":
-            #     tmp = np.ones_like(tmp)
-
-            if dbname in "velx vely velz":
-                tmp = scale(tmp)
->>>>>>> 41bfc6d777e409b93deb5fd8727c227541b17906
 
         for dbname, src, snk in zip('dens velx vely velz pres ener'.split(), srcs, snks):
             print("  %s  | % 12.5f % 12.5f  | % 12.5f % 12.5f" % (dbname, src.min(), snk.min(), src.max(), snk.max()))
