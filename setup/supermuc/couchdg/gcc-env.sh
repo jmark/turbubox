1e6f351193cfdaab4d1cf00a2d1e7ea7e8754e5e
Johannes Markert <markert@ph1.uni-koeln.de>
2017-10-25 16:00:55 +0200

    improving dir structure

---
+++ setup/supermuc/couchdg/gcc-env.sh
@@ -0,0 +1,26 @@
+#!/bin/sh -l
+
+set -e
+
+module purge
+
+source /etc/profile
+source /etc/profile.d/modules.sh
+
+module load gcc/7
+module load mpi.ompi/2.0/gcc
+#module load hdf5/mpi/1.8.17_gcc
+
+# OMPI="/opt/rrzk/lib/openmpi/2.1.1/gcc/bin"
+# 
+# export CC="${OMPI}/mpicc"
+# export CXX="${OMPI}/mpicc"
+# export FC="${OMPI}/mpifort"
+# export F9X="${OMPI}/mpif90"
+# export MPI_C_COMPILER="${OMPI}/mpicc"
+# export MPI_CXX_COMPILER="${OMPI}/mpicc"
+# export MPI_Fortran_COMPILER="${OMPI}/mpifort"
+# 
+# export LD_LIBRARY_PATH="$LD_LIBRARY_PATH:/home/jmarker2/builds/bin/hdf5-1.10.1/lib"
+
+exec "$@"
